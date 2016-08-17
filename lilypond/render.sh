b034a8db403590f78c7e2c214a7c1c3488b319ff
Mike iLL Kilmer <mike@mzoo.org>
2016-08-17 18:22:23 -0500

    New work in other languages.

---
+++ lilypond/render.sh
@@ -0,0 +1,5 @@
+arara hymnal.lytex
+pdflatex hymnal.tex
+find . -maxdepth 1 -type d -name '??' -exec rm -rf {} \;
+rm *.aux *.bcf *.dep *.out *.run.xml *.toc tmpx* snippet-map* snippet-names*
+open hymnal.pdf
