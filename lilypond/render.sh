c5d2a4ba917b8e2c41ff5a93b838d11028e7195f
Mike iLL Kilmer <mike@mzoo.org>
2016-08-17 21:00:34 -0500

    Use arara to render.

--- lilypond/render.sh
+++ lilypond/render.sh
@@ -1,5 +1,4 @@
-lilypond-book --latex-program=xelatex --pdf hymnal.lytex 
-xelatex hymnal.tex
+arara hymnal.lytex
 xelatex hymnal.tex
 find . -maxdepth 1 -type d -name '??' -exec rm -rf {} \;
 rm *.aux *.bcf *.dep *.out *.run.xml *.toc tmpx* snippet-map* snippet-names*
