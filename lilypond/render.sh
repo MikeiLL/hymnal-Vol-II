8d81c0506ee7740a2a615524b92cf2dd2e56ca23
Mike iLL Kilmer <mike@mzoo.org>
2016-08-17 20:44:10 -0500

    Not using arara, using xelatex.

--- lilypond/render.sh
+++ lilypond/render.sh
@@ -1,5 +1,6 @@
-arara hymnal.lytex
-pdflatex hymnal.tex
+lilypond-book --latex-program=xelatex --pdf hymnal.lytex 
+xelatex hymnal.tex
+xelatex hymnal.tex
 find . -maxdepth 1 -type d -name '??' -exec rm -rf {} \;
 rm *.aux *.bcf *.dep *.out *.run.xml *.toc tmpx* snippet-map* snippet-names*
 open hymnal.pdf
