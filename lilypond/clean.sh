42637f89401bd87cd6ed17d312549e068d7d503b
Mike iLL Kilmer <mike@mzoo.org>
2016-08-20 18:22:14 -0500

    Start of an index.

--- lilypond/clean.sh
+++ lilypond/clean.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
-
-arara sandbox.lytex
-xelatex sandbox.tex
+shopt -s extglob
+rm sandbox.!(lytex)
+rm hymnal.!(lytex)
 find . -maxdepth 1 -type d -name '??' -exec rm -rf {} \;
+rm tmp*
 rm *.aux *.bcf *.dep *.out *.run.xml *.toc tmpx* snippet-map* snippet-names*
-open sandbox.pdf
