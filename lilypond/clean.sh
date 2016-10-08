f19291feecadb86bc37c24af422a873aada66927
Mike iLL Kilmer <mike@mzoo.org>
2016-10-08 17:07:34 -0500

    add form valication destroy.

--- lilypond/clean.sh
+++ lilypond/clean.sh
@@ -3,4 +3,4 @@ rm sandbox.!(lytex)
 rm hymnal.!(lytex)
 find . -maxdepth 1 -type d -name '??' -exec rm -rf {} \;
 rm tmp*
-rm *.aux *.bcf *.dep *.out *.run.xml *.toc tmpx* snippet-map* snippet-names*
+rm *.aux *.bcf *.dep *.out *.run.xml *.toc tmpx* *.sxd *.sbx snippet-map* snippet-names*
